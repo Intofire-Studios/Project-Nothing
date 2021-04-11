0ae08965f55daefc24a184326071433e93e44621
rarmash <62994051+rarmash@users.noreply.github.com>
2021-04-11 11:56:56 +0300

    v.0.1ac

--- Assets/Scripts/MenuPause.cs
+++ Assets/Scripts/MenuPause.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 public class MenuPause : MonoBehaviour
 {
     [SerializeField] private GameObject Pause;
-    [SerializeField] private GameObject Lose;
     public void PauseOn(){
         Pause.SetActive(true);
         Time.timeScale = 0;
@@ -15,9 +14,6 @@ public class MenuPause : MonoBehaviour
     public void PauseOff(){
         Pause.SetActive(false);
         Time.timeScale = 1;
-    }
-    public void Loose(){
-        Lose.SetActive(true);
     }
     public void Quit(){
         SceneManager.LoadScene(0);
